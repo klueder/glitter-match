22c76a8fdb16b95b2630b9f4442a4d7f1bdcfbcd
klueder <kirsten.lueder@gmail.com>
2021-05-11 21:55:12 -0400

    cleaned up JS code for click function

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ console.log("test!")
 
 // var x = document.images[0].src
 
-// var pic1 = document.getElementById('pic1').src
-// var pic2 = document.getElementById('pic2').src
-// var pic3 = document.getElementById('pic3').src
-// var pic4 = document.getElementById('pic4').src
-// var pic5 = document.getElementById('pic5').src
-// var pic6 = document.getElementById('pic6').src
-// var pic7 = document.getElementById('pic7').src
-// var pic8 = document.getElementById('pic8').src
-console.log(pic2)
+// let cards = document.getElementsByClassName('glitter')
+// let cardsHolder = document.getElementsByClassName('glitterHolder')
+
+const pic1 = document.getElementById('pic1')
+var pic2 = document.getElementById('pic2')
+var pic3 = document.getElementById('pic3')
+var pic4 = document.getElementById('pic4')
+var pic5 = document.getElementById('pic5')
+var pic6 = document.getElementById('pic6')
+var pic7 = document.getElementById('pic7')
+var pic8 = document.getElementById('pic8')
+// console.log(pic2)
 
 let originalCardArray = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8]
 
@@ -62,73 +65,78 @@ randomPic()
 
 // Need function for click/"flip"
 
-// let cards = document.getElementsByClassName('glitter')
-// let cardsHolder = document.getElementsByClassName('glitterHolder')
+function picClick(x) {
+        document.getElementById(`pic${x}`).style.opacity = '1.0'
+}
 
-function picClick1() {
-    document.getElementById('pic1').style.opacity = '1.0'
-    document.getElementById('holder1').style.backgroundColor = ''
-}
-function picClick2() {
-    document.getElementById('pic2').style.opacity = '1.0'
-    document.getElementById('holder2').style.backgroundColor = ''
-}
-function picClick3() {
-    document.getElementById('pic3').style.opacity = '1.0'
-    document.getElementById('holder3').style.backgroundColor = ''
-}
-function picClick4() {
-    document.getElementById('pic4').style.opacity = '1.0'
-    document.getElementById('holder4').style.backgroundColor = ''
-}
-function picClick5() {
-    document.getElementById('pic5').style.opacity = '1.0'
-    document.getElementById('holder5').style.backgroundColor = ''
-}
-function picClick6() {
-    document.getElementById('pic6').style.opacity = '1.0'
-    document.getElementById('holder6').style.backgroundColor = ''
-}
-function picClick7() {
-    document.getElementById('pic7').style.opacity = '1.0'
-    document.getElementById('holder7').style.backgroundColor = ''
-}
-function picClick8() {
-    document.getElementById('pic8').style.opacity = '1.0'
-    document.getElementById('holder8').style.backgroundColor = ''
-}
-function picClick9() {
-    document.getElementById('pic9').style.opacity = '1.0'
-    document.getElementById('holder9').style.backgroundColor = ''
-}
-function picClick10() {
-    document.getElementById('pic10').style.opacity = '1.0'
-    document.getElementById('holder10').style.backgroundColor = ''
-}
-function picClick11() {
-    document.getElementById('pic11').style.opacity = '1.0'
-    document.getElementById('holder11').style.backgroundColor = ''
-}
-function picClick12() {
-    document.getElementById('pic12').style.opacity = '1.0'
-    document.getElementById('holder12').style.backgroundColor = ''
-}
-function picClick13() {
-    document.getElementById('pic13').style.opacity = '1.0'
-    document.getElementById('holder13').style.backgroundColor = ''
-}
-function picClick14() {
-    document.getElementById('pic14').style.opacity = '1.0'
-    document.getElementById('holder14').style.backgroundColor = ''
-}
-function picClick15() {
-    document.getElementById('pic15').style.opacity = '1.0'
-    document.getElementById('holder15').style.backgroundColor = ''
-}
-function picClick16() {
-    document.getElementById('pic16').style.opacity = '1.0'
-    document.getElementById('holder16').style.backgroundColor = ''
-}
+// need function for score 
+
+// function keepScore() {
+//     if ((document.getElementById('pic1').src == document.getElementById('pic2').src)
+//         && (document.getElementById('pic1').style.opacity = '1.0')
+//         && (document.getElementById('pic2').style.opacity = '1.0')) {
+//         document.getElementById('scoreNumber').innerHTML += 1
+//     } else if ((document.getElementById('pic1').src == document.getElementById('pic3').src) 
+//         && (document.getElementById('pic1').style.opacity = '1.0')
+//         && (document.getElementById('pic3').style.opacity = '1.0')) {
+//         document.getElementById('scoreNumber').innerHTML += 1
+//     } else if ((document.getElementById('pic1').src == document.getElementById('pic4').src) 
+//         && (document.getElementById('pic1').style.opacity = '1.0')
+//         && (document.getElementById('pic4').style.opacity = '1.0')){
+//         document.getElementById('scoreNumber').innerHTML += 1
+//     } else if ((document.getElementById('pic1').src == document.getElementById('pic5').src) 
+//         && (document.getElementById('pic1').style.opacity = '1.0')
+//         && (document.getElementById('pic5').style.opacity = '1.0')) {
+//         document.getElementById('scoreNumber').innerHTML += 1
+//     } else if (document.getElementById('pic1').src == document.getElementById('pic6').src) {
+//         document.getElementById('scoreNumber').innerHTML += 1
+//     } else if (document.getElementById('pic1').src == document.getElementById('pic7').src) {
+//         document.getElementById('scoreNumber').innerHTML += 1
+//     } else if (document.getElementById('pic1').src == document.getElementById('pic8').src) {
+//         document.getElementById('scoreNumber').innerHTML += 1
+//     } else if ((document.getElementById('pic1').src == document.getElementById('pic9').src) 
+//         && (document.getElementById('pic1').style.opacity = '1.0')
+//         && (document.getElementById('pic9').style.opacity = '1.0')) {
+//         document.getElementById('scoreNumber').innerHTML += 1
+//     } else if ((document.getElementById('pic1').src == document.getElementById('pic10').src) 
+//         && (document.getElementById('pic1').style.opacity = '1.0')
+//         && (document.getElementById('pic10').style.opacity = '1.0')) {
+//         document.getElementById('scoreNumber').innerHTML += 1
+//     } else if ((document.getElementById('pic1').src == document.getElementById('pic11').src) 
+//         && (document.getElementById('pic1').style.opacity = '1.0')
+//         && (document.getElementById('pic11').style.opacity = '1.0')){
+//         document.getElementById('scoreNumber').innerHTML += 1
+//     } else if ((document.getElementById('pic1').src == document.getElementById('pic12').src) 
+//         && (document.getElementById('pic1').style.opacity = '1.0')
+//         && (document.getElementById('pic12').style.opacity = '1.0')){
+//         document.getElementById('scoreNumber').innerHTML += 1
+//     } else if ((document.getElementById('pic1').src == document.getElementById('pic13').src) 
+//         && (document.getElementById('pic1').style.opacity = '1.0')
+//         && (document.getElementById('pic13').style.opacity = '1.0')) {
+//         document.getElementById('scoreNumber').innerHTML += 1
+//     } else if ((document.getElementById('pic1').src == document.getElementById('pic14').src) 
+//         && (document.getElementById('pic1').style.opacity = '1.0')
+//         && (document.getElementById('pic14').style.opacity = '1.0')) {
+//         document.getElementById('scoreNumber').innerHTML += 1
+//     } else if ((document.getElementById('pic1').src == document.getElementById('pic15').src) 
+//         && (document.getElementById('pic1').style.opacity = '1.0')
+//         && (document.getElementById('pic15').style.opacity = '1.0')) {
+//         document.getElementById('scoreNumber').innerHTML += 1
+//     } else if ((document.getElementById('pic1').src == document.getElementById('pic16').src) 
+//         && (document.getElementById('pic1').style.opacity = '1.0')
+//         && (document.getElementById('pic16').style.opacity = '1.0')){
+//         document.getElementById('scoreNumber').innerHTML += 1
+//     } else {
+//         console.log("error")
+//     }
+// }
+
+// keepScore()
+// need function for no more than two cards flipped at once (unless already matched)
+
+console.log('break')
+
+// Messy work below
 
 // function picClickBrokenProbably() {
 //     for (let i = 0; i < cardArray.length; i++) {
@@ -136,9 +144,6 @@ function picClick16() {
 //         originalCardArray[i].style.opacity = '1.0'
 //     }
 // }
-
-
-// Messy work below
 
 // cardsHolder.onclick = function () {
 //     console.log("click test - holder")
@@ -176,3 +181,55 @@ function picClick16() {
 // if function?? 
 
 
+//       picClick functions before drying up   
+// NEED TO DRY THIS CODE UP
+// function picClick1() {
+//     document.getElementById('pic1').style.opacity = '1.0'
+//     // document.getElementById('holder1').style.backgroundColor = ''
+// }
+// function picClick2() {
+//     document.getElementById('pic2').style.opacity = '1.0'
+//     // document.getElementById('holder2').style.backgroundColor = ''
+// }
+// function picClick3() {
+//     document.getElementById('pic3').style.opacity = '1.0'
+// }
+// function picClick4() {
+//     document.getElementById('pic4').style.opacity = '1.0'
+// }
+// function picClick5() {
+//     document.getElementById('pic5').style.opacity = '1.0'
+// }
+// function picClick6() {
+//     document.getElementById('pic6').style.opacity = '1.0'
+// }
+// function picClick7() {
+//     document.getElementById('pic7').style.opacity = '1.0'
+// }
+// function picClick8() {
+//     document.getElementById('pic8').style.opacity = '1.0'
+// }
+// function picClick9() {
+//     document.getElementById('pic9').style.opacity = '1.0'
+// }
+// function picClick10() {
+//     document.getElementById('pic10').style.opacity = '1.0'
+// }
+// function picClick11() {
+//     document.getElementById('pic11').style.opacity = '1.0'
+// }
+// function picClick12() {
+//     document.getElementById('pic12').style.opacity = '1.0'
+// }
+// function picClick13() {
+//     document.getElementById('pic13').style.opacity = '1.0'
+// }
+// function picClick14() {
+//     document.getElementById('pic14').style.opacity = '1.0'
+// }
+// function picClick15() {
+//     document.getElementById('pic15').style.opacity = '1.0'
+// }
+// function picClick16() {
+//     document.getElementById('pic16').style.opacity = '1.0'
+// }
