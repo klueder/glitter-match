bf01b31ea61a84d9e0f0db39255a3aecd294645f
klueder <kirsten.lueder@gmail.com>
2021-05-13 13:49:18 -0400

    more housekeeping in my JS

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// use console.dir if you're stuck
-
 // Array of 16 cards with two of each image
 
 const cards = [
@@ -121,16 +119,18 @@ randomPicPush()
 console.log(randomCardArray)
 randomPic()
 
-// Need func to stop flips after two 
+// Function to stop clicks after two cards are flipped
 
 function stopClick() {
+    let textBox = document.getElementById('textBox')
     if (flippedCardArray.length >= 2) {
         // ASK ABOUT THIS **************************
         document.getElementsByClassName('glitter').style.pointerEvents = 'none'
+        textBox.innerText = "You already clicked two!"
     } 
 }
 
-//             WORKING HERE *****************
+// Function to compare if cards match and let user know
 
 function compareCards() {
     let textBox = document.getElementById('textBox')
@@ -146,13 +146,8 @@ function compareCards() {
     }
 }
 
-// instead of stop click, make it compare the cards!!!!! then move on
-
-// NEED TO FIX SCORE FUNCTION
 // need function for score 
 
-// add if statements here
-// then add keepScore function to run after if the if statements are true
 
 // function keepScore() {
 //     for (var i=0, j=0; i<randomCardArrayA.length, j<randomCardArrayB.length; i++, j++) {
@@ -176,115 +171,4 @@ function picClick(x) {
     // keepScore()
 }
 
-// function picClickB(x) {
-//     stopClick()
-//     document.getElementById(`picB${x}`).style.opacity = '1.0';
-//     document.getElementById(`picB${x}`).dataset.switch = 'on'
-//     console.log(document.getElementById(`picB${x}`).dataset.switch)
-//     flippedCardArray.push(document.getElementById(`picB${x}`))
-//     compareCards()
-//     // keepScore()
-// }
-
-// keepScore()
-// need function for no more than two cards flipped at once (unless already matched)
-
 console.log("break")
-
-// Messy work below
-
-// function picClickBrokenProbably() {
-//     for (let i = 0; i < cardArray.length; i++) {
-//         console.log("did it work")
-//         originalCardArray[i].style.opacity = '1.0'
-//     }
-// }
-
-// cardsHolder.onclick = function () {
-//     console.log("click test - holder")
-//     cardsHolder.style.opacity = "0.0";
-//     cardsHolder.style.backgroundColor = null;
-//     cards.style.opacity = "1.0";
-// }
-
-// let header = document.querySelector('h1')
-// let holder1 = document.querySelector('#holder1')
-
-// header.onclick = function() {
-//     console.log("why")
-// }
-
-// pic1.onclick = function() {
-//     console.log("progress")
-// }
-
-// pic1.onclick = picClick();
-
-// pic1.addEventListener('click', picClick())
-
-// holder1.onclick = function() {
-//     console.log("or this progress")
-//     pic1.style.opacity = '1.0'
-// }
-
-// cards.onclick = function () {
-//     console.log("other click test - card only")
-//     cardsHolder.style.opacity = "0.0";
-//     cards.style.opacity = "1.0"
-// }
-
-// if function?? 
-
-
-//       picClick functions before drying up   
-// NEED TO DRY THIS CODE UP
-// function picClick1() {
-//     document.getElementById('pic1').style.opacity = '1.0'
-//     // document.getElementById('holder1').style.backgroundColor = ''
-// }
-// function picClick2() {
-//     document.getElementById('pic2').style.opacity = '1.0'
-//     // document.getElementById('holder2').style.backgroundColor = ''
-// }
-// function picClick3() {
-//     document.getElementById('pic3').style.opacity = '1.0'
-// }
-// function picClick4() {
-//     document.getElementById('pic4').style.opacity = '1.0'
-// }
-// function picClick5() {
-//     document.getElementById('pic5').style.opacity = '1.0'
-// }
-// function picClick6() {
-//     document.getElementById('pic6').style.opacity = '1.0'
-// }
-// function picClick7() {
-//     document.getElementById('pic7').style.opacity = '1.0'
-// }
-// function picClick8() {
-//     document.getElementById('pic8').style.opacity = '1.0'
-// }
-// function picClick9() {
-//     document.getElementById('pic9').style.opacity = '1.0'
-// }
-// function picClick10() {
-//     document.getElementById('pic10').style.opacity = '1.0'
-// }
-// function picClick11() {
-//     document.getElementById('pic11').style.opacity = '1.0'
-// }
-// function picClick12() {
-//     document.getElementById('pic12').style.opacity = '1.0'
-// }
-// function picClick13() {
-//     document.getElementById('pic13').style.opacity = '1.0'
-// }
-// function picClick14() {
-//     document.getElementById('pic14').style.opacity = '1.0'
-// }
-// function picClick15() {
-//     document.getElementById('pic15').style.opacity = '1.0'
-// }
-// function picClick16() {
-//     document.getElementById('pic16').style.opacity = '1.0'
-// }
