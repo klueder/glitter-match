8b7dd9b3f4395f7969293ab8bf2a7fa94c6223c9
klueder <kirsten.lueder@gmail.com>
2021-05-12 21:56:33 -0400

    very cleaned up code

--- script.js
+++ script.js
@@ -1,131 +1,158 @@
-console.log("test!")
+// use console.dir if you're stuck
 
-// var x = document.images[0].src
+// MAKE THE "CARDS" INTO OBJECTS with src and alt attached
 
-// let cards = document.getElementsByClassName('glitter')
-// let cardsHolder = document.getElementsByClassName('glitterHolder')
+const cards = [
+    {
+      "color": "hot pink",
+      "src": "/glitterJPGs/hotPink.jpg",
+      "alt": "hot pink glitter"
+    },
+    {
+        "color": "blue",
+        "src": "/glitterJPGs/blue.jpg",
+        "alt": "bright blue glitter"
+    },
+    {
+        "color": "iridescent",
+        "src": "/glitterJPGs/iridescent.jpg",
+        "alt": "iridescent glitter"
+    },
+    {
+        "color": "lime green",
+        "src": "/glitterJPGs/lime.jpg",
+        "alt": "lime green glitter"
+    },
+    {
+        "color": "purple",
+        "src": "/glitterJPGs/purple.jpg",
+        "alt": "purple glitter"
+    },
+    {
+        "color": "rainbow",
+        "src": "/glitterJPGs/rainbow.jpg",
+        "alt": "rainbow stripe glitter"
+    },
+    {
+        "color": "white",
+        "src": "/glitterJPGs/white.jpg",
+        "alt": "white glitter"
+    },
+    {
+        "color": "silver",
+        "src": "/glitterJPGs/silver.jpg",
+        "alt": "silver glitter"
+    }
+]
 
-const pic1 = document.getElementById('pic1')
-var pic2 = document.getElementById('pic2')
-var pic3 = document.getElementById('pic3')
-var pic4 = document.getElementById('pic4')
-var pic5 = document.getElementById('pic5')
-var pic6 = document.getElementById('pic6')
-var pic7 = document.getElementById('pic7')
-var pic8 = document.getElementById('pic8')
-// console.log(pic2)
 
-let originalCardArray = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8]
+// console.dir(cards[0])
+// console.log(cards[0].src)
 
-let imageURLS = ["/glitterJPGs/blue.jpg", "/glitterJPGs/hotPink.jpg", "/glitterJPGs/iridescent.jpg", "/glitterJPGs/lime.jpg","/glitterJPGs/purple.jpg", "/glitterJPGs/rainbow.jpg","/glitterJPGs/silver.jpg","/glitterJPGs/white.jpg"]
-
-let randomCardArray = []
-let secondRandomCardArray = []
+let randomCardArrayA = []
+let randomCardArrayB = []
+let flippedCardArray = []
+let imageURLS = [cards[0].src, cards[1].src, cards[2].src, cards[3].src, cards[4].src, cards[5].src, cards[6].src, cards[7].src]
 
 // Function for random img placement
 
-function randomPic() {
-    for (let i = 0; randomCardArray.length < 8; i++) {
-        let randomNum = Math.floor(Math.random() * 8)
+function randomPicA() {
+    for (let i = 0; randomCardArrayA.length < 8; i++) {
+        let randomNumA = Math.floor(Math.random() * 8)
         // if statement to ensure image only appears once per array
-        if (randomCardArray.includes(imageURLS[randomNum]) !== true) {
-        randomCardArray.push(imageURLS[randomNum])
-        } else {
-            console.log("error")
-        }
-        console.log(randomCardArray)
-        document.getElementById('pic1').src = randomCardArray[0];
-        document.getElementById('pic2').src = randomCardArray[1];
-        document.getElementById('pic3').src = randomCardArray[2];
-        document.getElementById('pic4').src = randomCardArray[3];
-        document.getElementById('pic5').src = randomCardArray[4];
-        document.getElementById('pic6').src = randomCardArray[5];
-        document.getElementById('pic7').src = randomCardArray[6];
-        document.getElementById('pic8').src = randomCardArray[7];
-    } for (let j=0; secondRandomCardArray.length < 8; j++) {
-        let secondRandomNum = Math.floor(Math.random() * 8)
+        while (randomCardArrayA.includes(imageURLS[randomNumA]) !== true) {
+        randomCardArrayA.push(imageURLS[randomNumA])
+        } 
+    for (let k = 0; k < randomCardArrayA.length; k++) {
+        document.getElementById(`picA${k}`).src = randomCardArrayA[k];
+    }
+    } 
+}
+function randomPicB() {
+    for (let j=0; randomCardArrayB.length < 8; j++) {
+        let randomNumB = Math.floor(Math.random() * 8)
         // if statement to ensure no duplicates in second array 
-        if (secondRandomCardArray.includes(imageURLS[secondRandomNum]) !== true) {
-        secondRandomCardArray.push(imageURLS[secondRandomNum])
-        } else {
-            console.log("error")
+        while (randomCardArrayB.includes(imageURLS[randomNumB]) !== true) {
+        randomCardArrayB.push(imageURLS[randomNumB])
         }
-        document.getElementById('pic9').src = secondRandomCardArray[0];
-        document.getElementById('pic10').src = secondRandomCardArray[1];
-        document.getElementById('pic11').src = secondRandomCardArray[2];
-        document.getElementById('pic12').src = secondRandomCardArray[3];
-        document.getElementById('pic13').src = secondRandomCardArray[4];
-        document.getElementById('pic14').src = secondRandomCardArray[5];
-        document.getElementById('pic15').src = secondRandomCardArray[6];
-        document.getElementById('pic16').src = secondRandomCardArray[7];
+    for (let l = 0; l < randomCardArrayB.length; l++) {
+        document.getElementById(`picB${l}`).src = randomCardArrayB[l];
+    }
     }
 }
 
-randomPic()
+randomPicA()
+randomPicB()
 
-// Need function for click/"flip"
+// Function for click/"flip"
 
-function picClick(x) {
-        document.getElementById(`pic${x}`).style.opacity = '1.0'
+function picClickA(x) {
+        document.getElementById(`picA${x}`).style.opacity = '1.0';
+        document.getElementById(`picA${x}`).dataset.switch = 'on'
+        console.log(document.getElementById(`picA${x}`).dataset.switch)
+        flippedCardArray.push(document.getElementById(`picA${x}`))
+        console.log(flippedCardArray)
+}
+function picClickB(x) {
+    document.getElementById(`picB${x}`).style.opacity = '1.0';
+    document.getElementById(`picB${x}`).dataset.switch = 'on'
+    console.log(document.getElementById(`picB${x}`).dataset.switch)
+    flippedCardArray.push(document.getElementById(`picB${x}`))
+    console.log(flippedCardArray)
 }
 
+// Need func to stop flips after two 
+
+// function stopClick() {
+//         if (flippedCardArray.length >= 2) {
+
+//         }
+    
+// }
+
+// NEED TO FIX SCORE FUNCTION
 // need function for score 
 
+// while loop?? (ex: while pic1 opacity = 1.0 && pic2 opacity = 1.0 && src == src etc etc )
+
+// if flippedCardArray >= 2 No more clicks 
+
+        // add if statements here
+        // then add keepScore function to run after if the if statements are true
+
 // function keepScore() {
-//     if ((document.getElementById('pic1').src == document.getElementById('pic2').src)
-//         && (document.getElementById('pic1').style.opacity = '1.0')
-//         && (document.getElementById('pic2').style.opacity = '1.0')) {
-//         document.getElementById('scoreNumber').innerHTML += 1
-//     } else if ((document.getElementById('pic1').src == document.getElementById('pic3').src) 
-//         && (document.getElementById('pic1').style.opacity = '1.0')
-//         && (document.getElementById('pic3').style.opacity = '1.0')) {
-//         document.getElementById('scoreNumber').innerHTML += 1
-//     } else if ((document.getElementById('pic1').src == document.getElementById('pic4').src) 
-//         && (document.getElementById('pic1').style.opacity = '1.0')
-//         && (document.getElementById('pic4').style.opacity = '1.0')){
-//         document.getElementById('scoreNumber').innerHTML += 1
-//     } else if ((document.getElementById('pic1').src == document.getElementById('pic5').src) 
-//         && (document.getElementById('pic1').style.opacity = '1.0')
-//         && (document.getElementById('pic5').style.opacity = '1.0')) {
-//         document.getElementById('scoreNumber').innerHTML += 1
-//     } else if (document.getElementById('pic1').src == document.getElementById('pic6').src) {
-//         document.getElementById('scoreNumber').innerHTML += 1
-//     } else if (document.getElementById('pic1').src == document.getElementById('pic7').src) {
-//         document.getElementById('scoreNumber').innerHTML += 1
-//     } else if (document.getElementById('pic1').src == document.getElementById('pic8').src) {
-//         document.getElementById('scoreNumber').innerHTML += 1
-//     } else if ((document.getElementById('pic1').src == document.getElementById('pic9').src) 
-//         && (document.getElementById('pic1').style.opacity = '1.0')
-//         && (document.getElementById('pic9').style.opacity = '1.0')) {
-//         document.getElementById('scoreNumber').innerHTML += 1
+//     if ((document.getElementById('pic1').src == document.getElementById('pic9').src) 
+//         && (document.getElementById('pic1').dataset.switch = 'on')
+//         && (document.getElementById('pic9').dataset.switch = 'on')) {
+//         document.getElementById('scoreNumber').innerText += 1
 //     } else if ((document.getElementById('pic1').src == document.getElementById('pic10').src) 
-//         && (document.getElementById('pic1').style.opacity = '1.0')
-//         && (document.getElementById('pic10').style.opacity = '1.0')) {
-//         document.getElementById('scoreNumber').innerHTML += 1
+//         && (document.getElementById('pic1').dataset.switch = 'on')
+//         && (document.getElementById('pic10').dataset.switch = 'on')) {
+//         document.getElementById('scoreNumber').innerText += 1
 //     } else if ((document.getElementById('pic1').src == document.getElementById('pic11').src) 
-//         && (document.getElementById('pic1').style.opacity = '1.0')
-//         && (document.getElementById('pic11').style.opacity = '1.0')){
-//         document.getElementById('scoreNumber').innerHTML += 1
+//         && (document.getElementById('pic1').dataset.switch = 'on')
+//         && (document.getElementById('pic11').dataset.switch = 'on')){
+//         document.getElementById('scoreNumber').innerText += 1
 //     } else if ((document.getElementById('pic1').src == document.getElementById('pic12').src) 
-//         && (document.getElementById('pic1').style.opacity = '1.0')
-//         && (document.getElementById('pic12').style.opacity = '1.0')){
-//         document.getElementById('scoreNumber').innerHTML += 1
+//         && (document.getElementById('pic1').dataset.switch = 'on')
+//         && (document.getElementById('pic12').dataset.switch = 'on')){
+//         document.getElementById('scoreNumber').innerText += 1
 //     } else if ((document.getElementById('pic1').src == document.getElementById('pic13').src) 
-//         && (document.getElementById('pic1').style.opacity = '1.0')
-//         && (document.getElementById('pic13').style.opacity = '1.0')) {
-//         document.getElementById('scoreNumber').innerHTML += 1
+//         && (document.getElementById('pic1').dataset.switch = 'on')
+//         && (document.getElementById('pic13').dataset.switch = 'on')) {
+//         document.getElementById('scoreNumber').innerText += 1
 //     } else if ((document.getElementById('pic1').src == document.getElementById('pic14').src) 
-//         && (document.getElementById('pic1').style.opacity = '1.0')
-//         && (document.getElementById('pic14').style.opacity = '1.0')) {
-//         document.getElementById('scoreNumber').innerHTML += 1
+//         && (document.getElementById('pic1').dataset.switch = 'on')
+//         && (document.getElementById('pic14').dataset.switch = 'on')) {
+//         document.getElementById('scoreNumber').innerText += 1
 //     } else if ((document.getElementById('pic1').src == document.getElementById('pic15').src) 
-//         && (document.getElementById('pic1').style.opacity = '1.0')
-//         && (document.getElementById('pic15').style.opacity = '1.0')) {
-//         document.getElementById('scoreNumber').innerHTML += 1
+//         && (document.getElementById('pic1').dataset.switch = 'on')
+//         && (document.getElementById('pic15').dataset.switch = 'on')) {
+//         document.getElementById('scoreNumber').innerText += 1
 //     } else if ((document.getElementById('pic1').src == document.getElementById('pic16').src) 
-//         && (document.getElementById('pic1').style.opacity = '1.0')
-//         && (document.getElementById('pic16').style.opacity = '1.0')){
-//         document.getElementById('scoreNumber').innerHTML += 1
+//         && (document.getElementById('pic1').dataset.switch = 'on')
+//         && (document.getElementById('pic16').dataset.switch = 'on')){
+//         document.getElementById('scoreNumber').innerText += 1
 //     } else {
 //         console.log("error")
 //     }
