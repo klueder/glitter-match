4895941832439f12205dd8343a956d7a64281b07
klueder <kirsten.lueder@gmail.com>
2021-05-14 22:41:48 -0400

    README file added

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# glitter-match
-card matching game 
+# Glitter Match 
+
+![gif of bernese mountain puppy in the snow](/glitterJPGs/berner.gif)
+
+#### A very sparkly card matching game 
+
+![screenshot of Glitter Match game](/glitterJPGs/glitter-match-screenshot.jpg)
+
+____
+
+#### Play it Here!
+
+
+
+___
+
+#### How to Play (& Win)
+
+- This game is (currently) a one-player game where the player clicks on cards with the goal of finding matches.
+- If the player finds a match (woohoo!), the game will notify the player and increase their score by one point. The matched cards will no longer be clickable and will stay flipped over so they are not confusing.
+- If the two flipped cards do not match, the game will notify the player and the player has the option to try again by clicking a button. If they click the button to continue and try again, the mis-matched cards will flip back over and the game will go on.
+-  The game is over and the player wins when all 8 matches have been found.
+
+___
+
+
+#### Coding Languages Used
+
+- HTML
+- CSS
+- JavaScript
+
+___
+
+#### User Stories
+
+###### MVP Goals
+
+- As a game player, I want to be able to flip the cards by clicking them, so that I can see if they match.
+- As a game player, I want the matches I have found to become un-playable for the rest of the game so that there are not errors with previously matched cards. 
+- As a game player, I want to receive credit/points for the matches I find so I do not have to keep track of the score myself. 
+- As a game player, I want the game to reset after I win so I can play again.
+- As a game player, I want to be notified if I accidentally try to mark two cards that do not match so I do not receive unfair points/credit.
+- As a game player, I want the game screen to be organized so it is not confusing while playing.
+
+###### Stretch Goals
+
+- As a game player, I want the game to look great so it is more pleasing to look at while playing.
+- As a game player, I would like the game to be timed (with a visual timer) so the game is more challenging.
+- As a game player, I would like the option to play the game with two players so I can play with my friends.
+- As a game player, I would like the game to have more than one level so I can challenge myself.
+
+___
+
+#### Wireframes
+
+Wireframe for before game start
+
+![wireframe showing a grid of gray squares](/glitterJPGs/wireframes0.jpg)
+
+Wireframe for mid-game
+
+![wireframe showing some squares have been flipped or matched and the score has been updated to 3 matches](/glitterJPGs/wireframes1.jpg)
+
+Wireframe for winning game!
+
+![wireframe showing user found all 8 matches and won the game](/glitterJPGs/wireframes2.jpg)
+
+___
+
+#### Approach to Making Glitter Match
+
+While I had an idea of what I would need to do to make this game, with it being my first big software engineering project, I definitely did not really know upfront what would be needed. There was a **lot** of trial and error, and I also underestimated how much time would be needed for this. I was still able to make a functional game, but I plan to continue with updates in the future. (See more below)
+
+___
+
+#### &#x1F534; Unresolved Issues, Hurdles Encountered, & Future Goals
+
+- I struggled to make the function for after two cards are flipped that do not match work. I ended up making a button so the user could "try again" after a mismatch. The button did work, but I would like to eventually make the game continue automatically.
+- I would like to add a timer, a two-player option, and more than one level. 
+- Some of the code is not as clean as I would like it and I plan to update that.
+
+___
--- script.js
+++ script.js
@@ -99,6 +99,8 @@ const cards = [
       }
 ]
 
+const pics = [document.getElementById('pic0'), document.getElementById('pic1'), document.getElementById('pic2'), document.getElementById('pic3'), document.getElementById('pic4'), document.getElementById('pic5'), document.getElementById('pic6'), document.getElementById('pic7'), document.getElementById('pic8'), document.getElementById('pic9'), document.getElementById('pic10'), document.getElementById('pic11'), document.getElementById('pic12'), document.getElementById('pic13'), document.getElementById('pic14'), document.getElementById('pic15')]
+
 // Array to hold cards after they are randomized
 let randomCardArray = []
 // Array to hold the random numbers to be used for randomization
@@ -107,6 +109,10 @@ let randomNumArray = []
 let flippedCardArray = []
 // Array to hold cards after they have been successfully matched
 let matchedCards = []
+// Array to hold matched cards so they do not affect the rest of the game
+let matchedCardsHold = []
+// Array to hold flipped pairs that didn't match
+let nonMatchCards = []
 
 // Function for random number
 function randomNumFunc() {
@@ -154,25 +160,14 @@ let foundAMatch = false;
 function picClick(x) {
     let textBox = document.getElementById('textBox')
     stopClick()
-    document.getElementById(`pic${x}`).style.opacity = '1.0';
+    // document.getElementById(`pic${x}`).style.opacity = '1.0';
+    pics[x].style.opacity = '1.0'
     document.getElementById(`pic${x}`).dataset.switch = 'on'
     flippedCardArray.push(document.getElementById(`pic${x}`))
     randomCardArray.pop(document.getElementById(`pic${x}`))
     compareCards()
 
-    // Function to change appearance/clickability of matched cards
-    function changeColor() {
-        for (let i= 0; i < cards.length; i++) {
-            if ((foundAMatch == true) && (matchedCards.length >= 2) && (document.getElementById(`pic${x}`).dataset.switch = 'on')) {
-                document.getElementById(`pic${x}`).style.opacity = '1.0'
-                document.getElementById(`pic${x}`).style.pointerEvents = 'none'
-                document.getElementById(`pic${x}`).dataset.switch = 'matched'
-                document.getElementById(`pic${x}`).style.visibility = 'hidden'
-                document.getElementById(`holder${x}`).style.backgroundColor = 'black'
-            } 
-        } 
-    }
-    setTimeout(changeColor, 1000)
+
 
     // Function to compare if cards match and let user know
     function compareCards() {
@@ -188,7 +183,7 @@ function picClick(x) {
                 matchedCards[0].dataset.switch = 'matched'
                 matchedCards[1].dataset.switch = 'matched'
                 foundAMatch = true
-                keepScore()
+                setTimeout(changeColor(), 500)
             } else if (flippedCardArray[0].src !== flippedCardArray[1].src) {
                 console.log("try again pal!")
                 button.style.visibility = 'visible'
@@ -197,34 +192,56 @@ function picClick(x) {
                 randomCardArray.push(flippedCardArray[1])
                 flippedCardArray[0].dataset.switch = 'off'
                 flippedCardArray[1].dataset.switch = 'off'
+                nonMatchCards.push(flippedCardArray[0])
+                nonMatchCards.push(flippedCardArray[1])
                 flippedCardArray.splice(0,2)
                 foundAMatch = false;
-
             }
         } 
     }
+    // Function to change appearance/clickability of matched cards
+    function changeColor() {
+        for (let i= 0; i < matchedCards.length; i++) {
+            if ((foundAMatch == true) && (matchedCards.length >= 2) && (document.getElementById(`pic${x}`).dataset.switch = 'matched')) {
+                matchedCards[0].style.opacity = '1.0'
+                matchedCards[0].style.pointerEvents = 'none'
+                matchedCards[1].style.opacity = '1.0'
+                matchedCards[1].style.pointerEvents = 'none'
+                matchedCardsHold.push(matchedCards[0])
+                matchedCardsHold.push(matchedCards[1])
+                matchedCards.splice(0,2)
+                keepScore()
+            } 
+        }
+        // setTimeout(changeColor(), 500)
+    }
 }
+
+
 
 // Function to keep score and let user know when they win the game
 function keepScore() {
+    for (let i=0; i < matchedCardsHold.length; i++) {
+        matchedCardsHold[i].pointerEvents = 'none'
+    }
     let scoreNumber = document.getElementById('scoreNumber')
     let textBox = document.getElementById('textBox')
-    if (matchedCards.length == 2) {
+    if (matchedCardsHold.length == 2) {
         scoreNumber.innerText = 1
-    } else if (matchedCards.length == 4) {
+    } else if (matchedCardsHold.length == 4) {
         scoreNumber.innerText = 2
-    } else if (matchedCards.length == 6) {
+    } else if (matchedCardsHold.length == 6) {
         scoreNumber.innerText = 3
-    } else if (matchedCards.length == 8) {
+    } else if (matchedCardsHold.length == 8) {
         scoreNumber.innerText = 4
         textBox.innerText = "Halfway there! Can you keep matching?"
-    } else if (matchedCards.length == 10) {
+    } else if (matchedCardsHold.length == 10) {
         scoreNumber.innerText = 5
-    } else if (matchedCards.length == 12) {
+    } else if (matchedCardsHold.length == 12) {
         scoreNumber.innerText = 6
-    } else if (matchedCards.length == 14) {
+    } else if (matchedCardsHold.length == 14) {
         scoreNumber.innerText = 7
-    } else if (matchedCards.length == 16) {
+    } else if (matchedCardsHold.length == 16) {
         scoreNumber.innerText = 8
         textBox.innerText = 'Hooray! You WIN!!!!!!'
         textBox.style.fontSize = '4rem'
@@ -237,13 +254,14 @@ function tryAgainButton() {
     let button = document.getElementById('tryAgain')
     button.style.visibility = 'hidden'
     textBox.innerText = 'Good choice! Keep looking!'
-    for (let i=0; i<cards.length; i++) {
-        if (document.getElementById(`pic${i}`).dataset.switch = 'off') {
-            document.getElementById(`pic${i}`).style.opacity = '0.0'
-            document.getElementById(`pic${i}`).style.pointerEvents = 'auto'
-        // } else if (document.getElementById(`pic${i}`).dataset.switch = 'matched') {
-        //     document.getElementById(`pic${i}`).style.opacity = '1.0'
-        //     document.getElementById(`pic${i}`).style.pointerEvents = 'none'
+    console.log(nonMatchCards)
+    for (let i=0; i<nonMatchCards.length; i++) {
+        if (nonMatchCards.length >= 2) {
+            if (nonMatchCards[i].dataset.switch !== 'matched') {
+            nonMatchCards[i].dataset.switch = 'off'
+            nonMatchCards[i].style.opacity = '0.0'
+            nonMatchCards[i].style.pointerEvents = 'auto'
+            }
         }
     }
 }
